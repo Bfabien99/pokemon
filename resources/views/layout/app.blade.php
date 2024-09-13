bd2589c6b62779f877f5e7d7fe15c845ad13c862
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-09-13 11:49:36 +0000

    app setup

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
+    <title>Pokedex</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
